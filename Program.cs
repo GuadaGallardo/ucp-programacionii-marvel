18631a18a68dca793b581d86d74daf559d5f2d90
GuadaGallardo <63385879+GuadaGallardo@users.noreply.github.com>
2020-10-14 10:03:44 -0300

    modificacion Herencia

--- Program.cs
+++ Program.cs
@@ -7,19 +7,12 @@ namespace trabajo_practico_3_programacion_ii
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenidos a mi juego MARVEL.");
-            Superpoder superpoder = new Superpoder();
-            Superheroe superheroe = new Superheroe(1,superpoder);
-            Villano villano = new Villano();
-            
-            Combate combate = new Combate(superheroe, villano, 3);
-
-            combate.Comenzar();
-
-
-
-            
-
-
+            Superheroe sup1 = new Superheroe("Spiderman", 1992, "Marvel", 100);
+            Villano vi1 = new Villano("Linterna verde", 1990, "Marvel", 80);
+            sup1.Atacar(vi1);
+            vi1.Atacar(sup1);
+        
         }
+
     }
 }
