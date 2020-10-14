7fa2dd633bed78f9ce18fb5b8c3c30901c253a5f
GuadaGallardo <63385879+GuadaGallardo@users.noreply.github.com>
2020-10-14 10:06:11 -0300

    modificacion Herencia

--- Villano.cs
+++ Villano.cs
@@ -2,8 +2,13 @@
 
 namespace trabajo_practico_3_programacion_ii
 {
-    class Villano
-    {
+    public class Villano : Personaje {
+
+    #region Constructor
+    public Villano(string nombre, int anioCreacion, string universo, int stamina) : base(nombre, anioCreacion,universo,stamina){
         
+    }
+
+    #endregion
     }
 }
